a30507cd5901d46fa02bbffedf97b53727bf7088
NikGitR <vindictam8@mail.ua>
2021-08-30 15:30:27 +0300

    roman

--- src/ToRoman.java
+++ src/ToRoman.java
@@ -20,6 +20,186 @@ public  class ToRoman {
             return "IX";
         } else if (arabic == (10)) {
             return "X";
+        }else if (arabic == (11)) {
+            return "XI";
+        }else if (arabic == (12)) {
+            return "XII";
+        }else if (arabic == (13)) {
+            return "XIII";
+        }else if (arabic == (14)) {
+            return "XIV";
+        }else if (arabic == (15)) {
+            return "XV";
+        }else if (arabic == (16)) {
+            return "XVI";
+        }else if (arabic == (17)) {
+            return "XVII";
+        }else if (arabic == (18)) {
+            return "XVIII";
+        }else if (arabic == (19)) {
+            return "XIX";
+        }else if (arabic == (20)) {
+            return "XX";
+        } else if (arabic == (21)) {
+            return "XXI";
+        } else if (arabic == (22)) {
+            return "XXII";
+        } else if (arabic == (23)) {
+            return "XXIII";
+        } else if (arabic == (24)) {
+            return "XXIV";
+        } else if (arabic == (25)) {
+            return "XXV";
+        } else if (arabic == (26)) {
+            return "XXVI";
+        } else if (arabic == (27)) {
+            return "XXVII";
+        } else if (arabic == (28)) {
+            return "XXVIII";
+        } else if (arabic == (29)) {
+            return "XXIX";
+        } else if (arabic == (30)) {
+            return "XXX";
+        }else if (arabic == (31)) {
+            return "XXXI";
+        }else if (arabic == (32)) {
+            return "XXXII";
+        }else if (arabic == (33)) {
+            return "XXXIII";
+        }else if (arabic == (34)) {
+            return "XXXIV";
+        }else if (arabic == (35)) {
+            return "XXXV";
+        }else if (arabic == (36)) {
+            return "XXXVI";
+        }else if (arabic == (37)) {
+            return "XXXVII";
+        }else if (arabic == (38)) {
+            return "XXXVIII";
+        }else if (arabic == (39)) {
+            return "XXXIX";
+        }else if (arabic == (40)) {
+            return "XL";
+        }else if (arabic == (41)) {
+            return "XLI";
+        }else if (arabic == (42)) {
+            return "XLII";
+        }else if (arabic == (43)) {
+            return "XLIII";
+        }else if (arabic == (44)) {
+            return "XLIV";
+        }else if (arabic == (45)) {
+            return "XLV";
+        }else if (arabic == (46)) {
+            return "XLVI";
+        }else if (arabic == (47)) {
+            return "XLVII";
+        }else if (arabic == (48)) {
+            return "XLVIII";
+        }else if (arabic == (49)) {
+            return "XLIX";
+        }else if (arabic == (50)) {
+            return "L";
+        }else if (arabic == (51)) {
+            return "LI";
+        }else if (arabic == (52)) {
+            return "LII";
+        }else if (arabic == (53)) {
+            return "LIII";
+        }else if (arabic == (54)) {
+            return "LIV";
+        }else if (arabic == (55)) {
+            return "LV";
+        }else if (arabic == (56)) {
+            return "LVI";
+        }else if (arabic == (57)) {
+            return "LVII";
+        }else if (arabic == (58)) {
+            return "LVIII";
+        }else if (arabic == (59)) {
+            return "LIX";
+        }else if (arabic == (60)) {
+            return "LX";
+        }else if (arabic == (61)) {
+            return "LXI";
+        }else if (arabic == (62)) {
+            return "LXII";
+        }else if (arabic == (63)) {
+            return "LXIII";
+        }else if (arabic == (64)) {
+            return "LXIV";
+        }else if (arabic == (65)) {
+            return "LXV";
+        }else if (arabic == (66)) {
+            return "LXVI";
+        }else if (arabic == (67)) {
+            return "LXVII";
+        }else if (arabic == (68)) {
+            return "LXVIII";
+        }else if (arabic == (69)) {
+            return "LXIX";
+        }else if (arabic == (70)) {
+            return "LXX";
+        }else if (arabic == (71)) {
+            return "LXXI";
+        }else if (arabic == (72)) {
+            return "LXXII";
+        }else if (arabic == (73)) {
+            return "LXXIII";
+        }else if (arabic == (74)) {
+            return "LXXIV";
+        }else if (arabic == (75)) {
+            return "LXXV";
+        }else if (arabic == (76)) {
+            return "LXXVI";
+        }else if (arabic == (77)) {
+            return "LXXVII";
+        }else if (arabic == (78)) {
+            return "LXXVIII";
+        }else if (arabic == (79)) {
+            return "LXXIX";
+        }else if (arabic == (80)) {
+            return "LXXX";
+        }else if (arabic == (81)) {
+            return "LXXXI";
+        }else if (arabic == (82)) {
+            return "LXXXII";
+        }else if (arabic == (83)) {
+            return "LXXXIII";
+        }else if (arabic == (84)) {
+            return "LXXXIV";
+        }else if (arabic == (85)) {
+            return "LXXXV";
+        }else if (arabic == (86)) {
+            return "LXXXVI";
+        }else if (arabic == (87)) {
+            return "LXXXVII";
+        }else if (arabic == (88)) {
+            return "LXXXVIII";
+        }else if (arabic == (89)) {
+            return "LXXXIX";
+        }else if (arabic == (90)) {
+            return "XC";
+        }else if (arabic == (91)) {
+            return "XCI";
+        }else if (arabic == (92)) {
+            return "XCII";
+        }else if (arabic == (93)) {
+            return "XCIII";
+        }else if (arabic == (94)) {
+            return "XCIV";
+        }else if (arabic == (95)) {
+            return "XCV";
+        }else if (arabic == (96)) {
+            return "XCVI";
+        }else if (arabic == (97)) {
+            return "XCVII";
+        }else if (arabic == (98)) {
+            return "XCVIII";
+        }else if (arabic == (99)) {
+            return "XCIX";
+        }else if (arabic == (100)) {
+            return "C";
         } else {
             return null;
         }
